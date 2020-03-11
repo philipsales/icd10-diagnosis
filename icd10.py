7e0b34aeff92b9257b212e133464e6d5e3c5e3e8
philipsales <pj_sal87@yahoo.com>
2020-03-11 21:29:54 +0800

    Add next_page functionality

--- icd10.py
+++ icd10.py
@@ -8,7 +8,19 @@ import pandas as pd
 class Icd10Spider(scrapy.Spider):
     name = 'icd10'
     allowed_domains = ['icd10data.com']
-    start_urls = ['https://www.icd10data.com/ICD10CM/Codes/C00-D49/C76-C80/C78-/C78.00']
+    base_url = 'https://www.icd10data.com' # with synyms
+    #start_urls = ['https://www.icd10data.com/ICD10CM/Codes/C00-D49/C76-C80/C78-/C78.00'] # with synyms
+    #start_urls = ['https://www.icd10data.com/ICD10CM/Codes/A00-B99/A00-A09/A06-/A06.0'] #without synyms but applicable to
+
+    pages = pd.read_csv('links.csv')
+    all_pages = pages['pages']
+    all_pages.drop_duplicates(keep=False, inplace=True)
+    page_links = list(all_pages)
+    print(page_links)
+
+    index = 0
+    start_urls = [ base_url + page_links[index]]
+
 
     def parse(self, response):
         self.log('Url: ' + response.url)
@@ -19,26 +31,19 @@ class Icd10Spider(scrapy.Spider):
 
         icd10_code = response.css('span.identifierDetail::text').extract_first(),
         icd10_diagnosis = response.css('h2.codeDescription::text').extract_first(),
-        icd10_synonyms = response.xpath('//span[contains(text(),"Approximate Synonyms")]/following-sibling::ul').get().strip()
-
-        yield self._set_standard_diagnosis(icd10_code, icd10_diagnosis)
-        #yield self._set_synonyms_diagnosis(icd10_code, icd10_synonyms)
+        icd10_synonyms = response.xpath('//span[contains(text(),"Approximate Synonyms")]/following-sibling::ul').get()
+        icd10_applicables = response.xpath('//span[contains(text(),"Applicable To")]/following-sibling::ul').get()
 
 
-    def _set_standard_diagnosis(self, icd10_code, icd10_diagnosis):
-        
-        standard_diagnosis = {
+        diagnosis = {
             'type': 'standard', 
             'code': icd10_code[0],
             'diagnosis': icd10_diagnosis[0]
         }
 
-        yield standard_diagnosis
+        yield diagnosis
 
-    def _set_synonyms_diagnosis(self, icd10_code, icd10_synonyms):
-        syn_list = []
         if icd10_synonyms:
-
             sp = StringProcessor(icd10_synonyms)
             synonyms = sp.dup().clean_html().split().remove_whitespace()
 
@@ -47,9 +52,48 @@ class Icd10Spider(scrapy.Spider):
                 synonym_diagnosis['type'] = 'synonyms' 
                 synonym_diagnosis['code'] = icd10_code[0]
                 synonym_diagnosis['diagnosis'] = synonym
-                return synonym_diagnosis
+                yield synonym_diagnosis
+
+
+
+        #if self.page_links[self.index]:
+        if self.index != len(self.page_links)-1:
+            next_link = self.page_links[self.index]
+            self.index += 1
+            new_url = self.base_url + next_link
+            yield scrapy.Request(url=new_url, callback=self.parse)
 
         
+        #yield self._set_nonstandard_diagnosis(icd10_code, icd10_synonyms)
+        #yield self._set_nonstandard_diagnosis(icd10_code, icd10_applicables)
+
+
+    def _set_standard_diagnosis(self, icd10_code, icd10_diagnosis):
+        print('------HELLO_-------')
+        diagnosis = {
+            'type': 'standard', 
+            'code': icd10_code[0],
+            'diagnosis': icd10_diagnosis[0]
+        }
+        print(diagnosis)
+
+        yield diagnosis
+
+        
+
+    def _set_nonstandard_diagnosis(self, icd10_code, nonstandard_diagnosis):
+        syn_list = []
+
+        if nonstandard_diagnosis:
+            sp = StringProcessor(nonstandard_diagnosis)
+            synonyms = sp.dup().clean_html().split().remove_whitespace()
+
+            for synonym in synonyms:
+                synonym_diagnosis = {}
+                synonym_diagnosis['type'] = 'synonyms' 
+                synonym_diagnosis['code'] = icd10_code[0]
+                synonym_diagnosis['diagnosis'] = synonym
+                yield synonym_diagnosis
 
 
 class StringProcessor(object):
